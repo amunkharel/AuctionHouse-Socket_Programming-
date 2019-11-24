adc018e08742ced96ac03ee09e061fb6faefd522
Shreeman Gautam <gautams@cs.unm.edu>
2019-11-23 18:36:46 -0700

    First instance of class Auction House.

--- src/finalProject/Main.java
+++ src/finalProject/Main.java
@@ -1,8 +1,18 @@
 package finalProject;
 
+import javafx.stage.Stage;
+
+import static javafx.application.Application.launch;
+
 public class Main {
 
+    AuctionHouse auctionHouse;
+
     public static void main(String[] args) {
-        System.out.println("Test");
+        launch(args);
+    }
+
+    public void start(Stage primaryStage) throws Exception {
+        auctionHouse = new AuctionHouse();
     }
 }
