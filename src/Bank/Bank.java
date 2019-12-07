8c0112489c47ae53dff5a0d3c4068fe34b05633e
Amun Kharel <akharel@cs.unm.edu>
2019-12-07 11:15:35 -0700

    Finish Program. Start commenting

--- src/Bank/Bank.java
+++ src/Bank/Bank.java
@@ -1,7 +1,9 @@
 package Bank;
 
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -11,7 +13,18 @@ public class Bank {
 
     public void startServer() {
         try {
-            ServerSocket serverSocket = new ServerSocket(8888);
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            String bankPortNumber = "";
+            boolean isLegalPort = false;
+            while(!isLegalPort){
+                System.out.println("Type Bank's port number: ");
+                bankPortNumber = br.readLine();
+                if(isInteger(bankPortNumber)){
+                    isLegalPort = true;
+                }
+            }
+
+            ServerSocket serverSocket = new ServerSocket(Integer.parseInt(bankPortNumber));
 
             System.out.println("Bank Server Started...");
 
@@ -27,5 +40,16 @@ public class Bank {
             e.printStackTrace();
         }
 
+    }
+
+    public boolean isInteger(String str){
+        try{
+            Integer.parseInt(str);
+        } catch(NumberFormatException e){
+            return false;
+        } catch (NullPointerException e){
+            return false;
+        }
+        return true;
     }
 }
